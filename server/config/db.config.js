47cbef5aa531d7282c8dd56f254b4a61ec1d0e43
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-10-09 11:38:00 +0500

    db connection url pasted in env

--- server/config/db.config.js
+++ server/config/db.config.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
-
+require('dotenv').config()
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/TodoDB', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true })
     .then(success => console.log("Connection Established"))
     .catch(failure => console.log("Error occured while connceting mongoDB " + failure))
 
