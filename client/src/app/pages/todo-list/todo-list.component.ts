1406b695ac84536c5c01e0ceeaf9ba4d5f1f2995
Aman Sultan Baig <amansultanbaig@gmail.com>
2020-10-12 23:17:05 +0500

    add todo api integrated with email and phone uniquness checked

--- client/src/app/pages/todo-list/todo-list.component.ts
+++ client/src/app/pages/todo-list/todo-list.component.ts
@@ -10,16 +10,22 @@ import { NzMessageService } from 'ng-zorro-antd/message';
 })
 
 export class TodoListComponent implements OnInit {
-  isVisible = false;
-  isVisible2 = false;
+  isVisible: boolean = false;
+  editVisible: boolean = false;
+  addVisible: boolean = false;
   isOkLoading = false;
   oneTodo;
   getAllTodos;
   confirmModal?: NzModalRef; // For testing by now
+
   UpdateName = ''
   UpdateEmail = ''
   UpdatePhone = ''
   copyId = ''
+
+  add_name = ''
+  add_email = ''
+  add_phone = ''
 
   constructor(private _api: RestApiService, private modal: NzModalService, private message: NzMessageService) { }
 
@@ -35,24 +41,34 @@ export class TodoListComponent implements OnInit {
     })
   }
 
+  getTodoByID(id) {
+    this._api.getTodoById(id).subscribe(todo => {
+      this.oneTodo = todo;
+      this.oneTodo = this.oneTodo.data;
+    })
+  }
+
   // view todo modal
   showModal(todoId, flag) {
     this.copyId = todoId;
-    this._api.getTodoById(todoId).subscribe(todo => {
-      this.oneTodo = todo;
-      this.oneTodo = this.oneTodo.data;
-      if (flag === 'view-state') {
-        this.isVisible = true
-      } else if (flag === 'edit-state') {
-        this.isVisible2 = true
-      }
-    })
+    if (flag === 'view-state' && todoId) {
+      this.getTodoByID(todoId)
+      this.isVisible = true
+    }
+    else if (flag === 'edit-state' && todoId) {
+      this.getTodoByID(todoId)
+      this.editVisible = true
+    }
+    else if (flag === 'add-state' && !todoId) {
+      this.addVisible = true
+    }
   }
 
   handleOk(flag): void {
     if (flag == 'view-ok') {
       this.isVisible = false
-    } else if (flag == 'edit-ok') {
+    }
+    else if (flag == 'edit-ok') {
       let body = {
         Name: this.UpdateName,
         Email: this.UpdateEmail,
@@ -60,17 +76,41 @@ export class TodoListComponent implements OnInit {
       }
       this._api.editTodo(body, this.copyId).subscribe(todo => {
         this.message.success('Updated Successfully!!!');
-        this.isVisible2 = false;
+        this.editVisible = false;
+        // displaying added todos
         this.getTodos()
       })
+    }
+    else if (flag == 'add-ok') {
+      let body = {
+        Name: this.add_name,
+        Email: this.add_email,
+        Phone: this.add_phone,
+      }
+      this._api.addTodo(body).subscribe(todo => {
+        this.message.success('Added  Successfully!!!');
+        this.addVisible = false;
+        // displaying added todos
+        this.getTodos();
+        this.resetForm()
+      }, (err: any) => {
+        if (err.error.message == "Email Already Exists") {
+          this.message.error(err.error.message)
+        } else if(err.error.message == "Phone Already Exists") {
+          this.message.error(err.error.message)
+        }
+      })
+
     }
   }
 
   handleCancel(flag): void {
     if (flag == 'view-cancel') {
       this.isVisible = false
-    } else if ('edit-cancel') {
-      this.isVisible2 = false
+    } else if (flag == 'edit-cancel') {
+      this.editVisible = false
+    } else if (flag == 'add-cancel') {
+      this.addVisible = false
     }
   }
 
@@ -95,5 +135,11 @@ export class TodoListComponent implements OnInit {
     });
   }
 
+  // reset add todo form fields
+  resetForm() {
+    this.add_name = null;
+    this.add_email = null;
+    this.add_phone = null;
+  }
 
 }
